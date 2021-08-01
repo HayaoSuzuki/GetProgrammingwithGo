50a882ec310060b3da4b82126d239b393d2ce24b
Hayao Suzuki <hayao.math@gmail.com>
2021-08-01 23:54:02 +0900

    mars.goを追加
    火星では17歳と聞いて若干せつなくなった。

---
+++ lesson02/mars.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("火星の表面で、私の体重は")
+	fmt.Print(110.0 * 0.3787)
+	fmt.Print("キログラム、年齢は")
+	fmt.Print(32 * 365 / 687)
+	fmt.Print("歳になるでしょう。")
+}
