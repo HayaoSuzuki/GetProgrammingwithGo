4b0d80e2cce167dd28f36373dcff5f4f73c4e711
Hayao Suzuki <hayao.math@gmail.com>
2021-08-02 08:04:09 +0900

    countdown.goを1/100の確率で発射中止になるように修正
    縁起が悪い

--- lesson03/countdown.go
+++ lesson03/countdown.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -10,7 +11,13 @@ func main() {
 	for count > 0 {
 		fmt.Println(count)
 		time.Sleep(time.Second)
+		if rand.Float64() <= 0.1 {
+			fmt.Println("FAIL!")
+			break
+		}
 		count--
 	}
-	fmt.Println("Liftoff!")
+	if count == 0 {
+		fmt.Println("Liftoff!")
+	}
 }
