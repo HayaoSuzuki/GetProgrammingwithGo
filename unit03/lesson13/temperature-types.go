0a4c3744ef6e7bc75d16894396fe1eb153c45d4f
Hayao Suzuki <hayao.math@gmail.com>
2021-08-09 13:27:35 +0900

    新しい型を宣言するその2
    自然にメソッドに誘導する流れ

---
+++ unit03/lesson13/temperature-types.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type celsius float64
+type kelvin float64
+
+func kelvinToCelsius(k kelvin) celsius {
+	return celsius(k - 273.15)
+}
+
+func celsiusToKelvin(c celsius) kelvin {
+	return kelvin(c + 273.15)
+}
+
+func main() {
+	var k kelvin = 294.0
+	c := kelvinToCelsius(k)
+	fmt.Println(k, "Kは", c, "Cです。")
+
+	var moonC celsius = 127.0
+	moonK := celsiusToKelvin(moonC)
+	fmt.Println("月の表面温度", moonC, "Cは、", moonK, "Kです。")
+
+}
